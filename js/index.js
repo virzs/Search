efc0d8680eb60bea79e906c79d720de36e7c64f9
lcl <lclluochaoli@163.com>
2019-11-25 13:23:02 +0800

    增加两个百度搜索相关方法（测试）

--- js/index.js
+++ js/index.js
@@ -21,3 +21,66 @@ request.onload = function () {
     });
     engine.innerHTML = html
 }
+// 百度搜索参数测试
+
+var txt = document.getElementById("text");
+var oUl = document.getElementById("list");
+txt.onkeyup = function () {
+    var val = txt.value;
+    var oScript = document.createElement("script"); //动态创建script标签 
+    oScript.src = "https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd=" + val + "&cb=callback";
+    //添加链接及回调函数 
+    document.body.appendChild(oScript); //添加script标签 
+    document.body.removeChild(oScript); //删除script标签 
+}
+//回调函数 
+function callback(data) {
+    var str = "";
+    for (var i = 0; i < data.s.length; i++) {
+        str += "<li><a href=\"https://www.baidu.com/s?wd=" + data.s[i] + "\">" + data.s[i] + "</a></li>";
+    }
+    //console.log(str); 
+    oUl.innerHTML = str;
+    oUl.style.display = "block";
+}
+
+
+function myHandle(data) {
+    console.log(data)
+}
+
+//封装的jsonp函数（url为传入的地址，data为参数对象，callback为回调函数）
+function myJsonp(url, data, callback) {
+    let params = '';
+    url += url.indexOf('?') > -1 ? '&' : '?';
+    for (var key in data) {
+        params += '&' + key + '=' + data[key];
+    }
+    url += params;
+
+    //cb为百度后端回调函数的键名，后面接上你的回调函数名
+    url += '&' + 'cb=' + callback
+    let script = document.createElement('script')
+    script.setAttribute('src', url)
+    document.body.appendChild(script);
+    document.body.removeChild(script);
+
+}
+//当页面加载完毕，调用函数
+window.onload = function () {
+
+    //获取元素
+    var target = document.getElementById('target');
+
+    //当键盘抬起时触发jsonp函数
+    target.onkeyup = function () {
+        let value = target.value.trim()
+
+        if (!value) return;
+
+        // 调用封装的jsonp函数
+        myJsonp('https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su', {
+            "wd": value
+        }, 'myHandle')
+    };
+};
