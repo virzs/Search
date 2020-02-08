d22684c27634ba09ea13d64bcb8abfca7e3f8524
virs <virs2018@outlook.com>
2020-02-08 18:31:08 +0800

    本地存储操作模块

---
+++ js/module/storage.func.mjs
@@ -0,0 +1,15 @@
+//设置本地存储
+function setStorage(name, value) {
+    window.localStorage.setItem(name, value);
+}
+
+// 获取本地存储内容
+function getStorage(key) {
+    let value = window.localStorage.getItem(key);
+    return value;
+}
+
+export {
+    setStorage,
+    getStorage
+}
