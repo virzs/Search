a9678d330b729f30d420e1592c4d643257c010a7
virs <virs2018@outlook.com>
2020-02-10 21:17:04 +0800

    添加模态框模块

---
+++ js/module/dialog.func.mjs
@@ -0,0 +1,46 @@
+import {
+    body
+} from "./dom.constant.mjs";
+
+function openDialog(data) {
+    let [title, content, btns] = ["", "", "", ""];
+    let change = "";
+    if (data == undefined || data.title == undefined) {
+        title = "提示";
+    } else {
+        title = data.title;
+    }
+    data.content.forEach(item => {
+        if (item.defaultValue !== "") {
+            change = item.defaultValue
+        }
+        if (item.type == "input") {
+            content += `<div id="${item.value}Dialog"><span class="content-label">${item.name}：</span><input placeholder="请输入${item.name}" value="${item.defaultValue}" /></div>`;
+        } else if (item.type == "text") {
+            content += `<div><span class="content-label">${item.name}：</span><p>${item.value}</p></div>`;
+        }
+    })
+    data.button.forEach(item => {
+        btns += `<span><button id="${item.value}Dialog">${item.name}</button></span>`
+    })
+    let dialog = `
+        <div id="dialog">
+            <div class="dialog-header">${title}</div>
+            <div class="dialog-body">${content}</div>
+            <div class="dialog-footer">${btns}</div>
+        </div>`;
+    let dialogWrapper = document.createElement("div");
+    dialogWrapper.setAttribute("id", "dialogWrapper");
+    dialogWrapper.innerHTML = dialog;
+    body.appendChild(dialogWrapper);
+}
+
+function closeDialog() {
+    let dialog = document.querySelector("#dialogWrapper");
+    dialog.remove();
+}
+
+export {
+    openDialog,
+    closeDialog
+}
