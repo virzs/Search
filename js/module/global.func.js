c5e447b3ca8478e57ea85d567906fcf177814a49
virs <virs2018@outlook.com>
2020-02-16 12:38:49 +0800

    增加随机颜色

--- js/module/global.func.js
+++ js/module/global.func.js
@@ -26,7 +26,12 @@ function findSettingInfo(value) {
     }
     return href;
 }
+
+function getRandomColor() {
+    return '#' + Math.random().toString(16).slice(2, 8)
+}
 export {
     stopPropagation,
-    findSettingInfo
+    findSettingInfo,
+    getRandomColor
 }
