aa4cf55ef7959df8f2e52248383fc664f968c5b8
virzs <2369310227@qq.com>
2020-03-06 16:32:39 +0800

    增加获取api数据

--- js/module/all.data.js
+++ js/module/all.data.js
@@ -1,18 +1,30 @@
 var jsonData = {}; //获取的json文件数据
-var url = "../../data/index.json"; //json文件路径
+var apiData = [];
+var mainUrl = "../../data/mainData.json"; //json文件路径
+var apiUrl = "../../data/apiData.json"; //api文件路径
 
 /*
     ajax同步获取json文件数据
  */
 $.ajax({
     type: "get",
-    url: url,
+    url: mainUrl,
     dataType: "json",
     async: false,
-    success: function (response) {
-        jsonData = response;
+    success: function (res) {
+        jsonData = res;
     }
 });
+$.ajax({
+    type: "get",
+    url: apiUrl,
+    dataType: "json",
+    async: false,
+    success: function (res) {
+        apiData = res;
+    }
+})
 export {
-    jsonData
+    jsonData,
+    apiData
 }
