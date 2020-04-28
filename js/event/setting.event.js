f4e54873619bef5ed411fca4851dff48badb269d
virzs <2369310227@qq.com>
2020-04-28 23:17:35 +0800

    添加防重事件

--- js/event/setting.event.js
+++ js/event/setting.event.js
@@ -3,16 +3,28 @@ import {
     loadHitokoto,
     hideSentence
 } from "../module/sentence.func.js";
+
 import {
     setStorageBefore
 } from "../module/animation.func.js";
+
 import {
     showLogo,
     showTime,
     closeLogo
 } from "../module/title.func.js";
 
+import {
+    getStorage
+} from "../module/storage.func.js";
+
+import {
+    openMessage
+} from "../components/message.component.js";
+
 export const sentenceSetting = (value, callback, state) => {
+    let error = eventError('sentence', value, state);
+    if (!error && state) return;
     let func = () => {
         if (value == 'jinrishici') loadJinrishiji(callback);
         if (value == 'hitokoto') loadHitokoto();
@@ -26,20 +38,30 @@ export const sentenceSetting = (value, callback, state) => {
 }
 
 export const logoSetting = (value, state) => {
+    let error = eventError('logo', value, state);
+    if (!error && state) return;
     let func = () => {
-        if (value == 'textLogo') {
-            showLogo();
-        }
-        if (value == 'timeLogo') {
-            showTime();
-        }
-        if (value == 'closeLogo') {
-            closeLogo();
-        }
+        if (value == 'textLogo') showLogo();
+        if (value == 'timeLogo') showTime();
+        if (value == 'closeLogo') closeLogo();
     }
     if (state) {
         setStorageBefore(func, 'logo', value);
     } else {
         func();
+    }
+}
+
+const eventError = (name, value, state) => {
+    let storage = getStorage(name)
+    if (storage.value == value && state) {
+        openMessage({
+            title: "提示",
+            type: "error",
+            content: `请勿重复选择！！！`
+        })
+        return false;
+    } else {
+        return true;
     }
 }
