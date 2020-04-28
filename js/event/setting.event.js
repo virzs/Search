0d992972505e31fb8d5872e8995c3d91db42e945
virzs <2369310227@qq.com>
2020-04-28 16:39:19 +0800

    增加设置Logo样式

--- js/event/setting.event.js
+++ js/event/setting.event.js
@@ -6,6 +6,11 @@ import {
 import {
     setStorageBefore
 } from "../module/animation.func.js";
+import {
+    showLogo,
+    showTime,
+    closeLogo
+} from "../module/title.func.js";
 
 export const sentenceSetting = (value, callback, state) => {
     let func = () => {
@@ -15,6 +20,25 @@ export const sentenceSetting = (value, callback, state) => {
     }
     if (state) {
         setStorageBefore(func, 'sentence', value);
+    } else {
+        func();
+    }
+}
+
+export const logoSetting = (value, state) => {
+    let func = () => {
+        if (value == 'textLogo') {
+            showLogo();
+        }
+        if (value == 'timeLogo') {
+            showTime();
+        }
+        if (value == 'closeLogo') {
+            closeLogo();
+        }
+    }
+    if (state) {
+        setStorageBefore(func, 'logo', value);
     } else {
         func();
     }
--- js/module/title.func.js
+++ js/module/title.func.js
@@ -6,9 +6,12 @@ import {
     jsonData
 } from './all.data.js';
 
-export const showLogo = (status = true) => {
+let t = null;
+
+export const showLogo = () => {
+    clearTimeout(t);
     let info = jsonData.info;
-    if (!status) return;
+    searchLogo.style.display = 'flex';
     if (info.logo == '') {
         searchLogo.innerHTML = `
             <div class="text-logo">
@@ -29,11 +32,35 @@ export const showTime = () => {
     let HOUR = DATE.getHours(); //小时
     let MINUTE = DATE.getMinutes(); //分钟
     let SECOND = DATE.getSeconds(); //秒
+
+    MONTH = checkTime(MONTH);
+    DAY = checkTime(DAY);
+    HOUR = checkTime(HOUR);
+    MINUTE = checkTime(MINUTE);
+    SECOND = checkTime(SECOND);
+
+    searchLogo.style.display = 'flex';
     searchLogo.innerHTML = `
         <div class="time-logo">
-           <span>${HOUR}</span>
-           <span>${MINUTE}</span>
-           <span>${SECOND}</span>
+           <p>
+                <span>${HOUR}</span>
+                <span>${MINUTE}</span>
+                <span>${SECOND}</span>
+           </p>
+           <p>
+                <span>${YEAR}</span>
+                <span>${MONTH}</span>
+                <span>${DAY}</span>
+            </p>
         </div>`;
-    let t = setTimeout(showTime, 1000);
+    t = setTimeout(showTime, 1000);
+}
+
+export const closeLogo = () => {
+    clearTimeout(t);
+    searchLogo.style.display = 'none';
+}
+
+const checkTime = (value) => {
+    return value < 10 ? `0${value}` : value;
 }
