6edde24b9184bb3ea54a69ebdb5667fa7820bfff
virzs <2369310227@qq.com>
2020-05-01 21:01:12 +0800

    调整设置项

--- js/event/setting.event.js
+++ js/event/setting.event.js
@@ -22,6 +22,70 @@ import {
     openMessage
 } from "../components/message.component.js";
 
+import {
+    findSettingInfo
+} from "../module/global.func.js";
+
+import {
+    changeUI
+} from "../module/ui.func.js";
+
+import {
+    changeSkin
+} from "../module/skin.func.js";
+
+import {
+    setCustomizeImage,
+    setBingImage,
+    setdefault
+} from "../module/bg.func.js";
+
+//背景设置
+export const bgSetting = (value, state) => {
+    let error = eventError('bg', value, state);
+    if (!error && state) return;
+    let func = () => {
+        if (value == 'changebg') setCustomizeImage();
+        if (value == 'setBingImage') setBingImage(false);
+        if (value == 'setdefault') setdefault(value);
+    }
+    if (state) {
+        setStorageBefore(func, 'bg', value);
+    } else {
+        func();
+    }
+}
+
+//配色设置
+export const skinSetting = (value, state) => {
+    let error = eventError('skin', value, state);
+    let href = findSettingInfo(value);
+    if (!error && value !== './css/skin/skin_Transparent.css' && state) return;
+    let func = () => {
+        changeSkin(href);
+    }
+    if (state) {
+        setStorageBefore(func, 'skin', href);
+    } else {
+        func();
+    }
+}
+
+//UI设置
+export const uiStyleSetting = (value, state) => {
+    let error = eventError('uiStyle', value, state);
+    let href = findSettingInfo(value);
+    if (!error && state) return;
+    let func = () => {
+        changeUI(href);
+    }
+    if (state) {
+        setStorageBefore(func, 'uiStyle', value);
+    } else {
+        func();
+    }
+}
+
 //语句设置
 export const sentenceSetting = (value, callback, state) => {
     let error = eventError('sentence', value, state);
@@ -56,7 +120,7 @@ export const logoSetting = (value, state) => {
 
 //重复设置事件
 const eventError = (name, value, state) => {
-    let storage = getStorage(name)
+    let storage = getStorage(name);
     if (storage.value == value && state) {
         openMessage({
             title: "提示",
