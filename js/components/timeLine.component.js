61d3d691734732367490d674b41117a489c176df
virzs <2369310227@qq.com>
2020-05-06 15:58:19 +0800

    增加更新记录函数

---
+++ js/components/timeLine.component.js
@@ -0,0 +1,28 @@
+export const timeLine = (value, order = 'positive') => {
+    //positive正序
+    //inverted倒序
+    let html = '';
+    let timeLineData = [];
+    value.forEach(item => {
+        if (order == 'inverted') {
+            timeLineData.unshift(item);
+        } else {
+            timeLineData.push(item);
+        }
+    })
+    timeLineData.forEach((item, index) => {
+        let time = `<div class="item-title"> ${item.time}</div>`;
+        let content = '';
+        item.content.forEach((inner, i) => {
+            content += `
+                <p class="item-content">
+                    <span class="label ${inner.type}">
+                        ${inner.type}
+                    </span>
+                    <span>${inner.content}</span>
+                </p>`;
+        })
+        html += `<div class="timeLine-item">${time}${content}</div>`;
+    })
+    return html;
+}
