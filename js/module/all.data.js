dd9fb7ef3ecca3399e9a689d132eb878e9278b53
virzs <2369310227@qq.com>
2020-05-06 15:58:34 +0800

    增加获取更新记录

--- js/module/all.data.js
+++ js/module/all.data.js
@@ -1,7 +1,9 @@
 var jsonData = {}; //获取的json文件数据
 var apiData = [];
+var updateData = [];
 var mainUrl = "../../data/mainData.json"; //json文件路径
 var apiUrl = "../../data/apiData.json"; //api文件路径
+var updateUrl = '../../data/updateData.json'; //更新记录
 
 /*
     ajax同步获取json文件数据
@@ -24,7 +26,17 @@ $.ajax({
         apiData = res;
     }
 })
+$.ajax({
+    type: "get",
+    url: updateUrl,
+    dataType: "json",
+    async: false,
+    success: function (res) {
+        updateData = res;
+    }
+})
 export {
     jsonData,
-    apiData
+    apiData,
+    updateData
 }
